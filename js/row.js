428f68845a5bcbe2029ed0af78304321feb2aeaf
feruz-bro <dzurakulovferuz5@gmail.com>
2024-07-15 18:29:09 +0500

    first commit

---
+++ js/row.js
@@ -0,0 +1,55 @@
+
+// let guestList = 'Putin Zelenskiy Biden Trump Erdogan Mirziyoyev Obama Lukashenko'
+
+// let name = prompt('What is your name?')
+
+// if(guestList.toLocaleLowerCase().includes(name.toLocaleLowerCase())){
+//     alert(`Welcome`+ name)
+
+// }else{
+//     alert(`You are not welcome` + name)
+// }
+
+
+
+// let age = prompt(`How old are you ?`)
+
+// if( age >= 0 && age <= 7){
+//     alert(`Малой`)
+
+// }else if (age >= 8 && age <= 13){
+// alert(`Братишка`)
+
+// }else if (age >= 14 && age <= 20){
+//     alert(`Подросток`)
+
+// }else if (age >= 21 && age <= 35){
+//     alert(`Зрелый`)
+// }else if (age >= 36 && age <= 60){
+//     alert(`Средний возраст`)
+// }
+
+
+// спрашиваете номер месяца программа должна выдать какой это сезон 
+// спрашиваете дату и прорамма выдает какая декада 
+// калькулятор
+
+// 
+
+
+
+
+
+let decade = prompt(`Какой сейчас декада ?`)
+
+if (decade >= 1 && decade <= 10 ){
+    alert(`Первый декада`)
+} else if  ( decade >= 11 && decade <= 20){
+    alert(`Второй декада`)
+}else if  ( decade >= 21 && decade <= 31){
+    alert(`Третий декада`)
+}
+
+
+
+
